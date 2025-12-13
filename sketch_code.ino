d048b8b37de661d6d8fcd0877d1d97bf640dfb9f
Артём Ходько <artemhodko178@gmail.com>
2025-12-13 17:48:33 +0300

    code

---
+++ sketch_code.ino
@@ -0,0 +1,42 @@
+#include <Servo.h>
+
+#define TRIG_PIN 9
+#define ECHO_PIN 10
+#define SERVO_PIN 11
+
+
+
+
+void setup() {
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  manipulator.attach(SERVO_PIN);
+  manipulator.write(90);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  int distance = getDistance(TRIG_PIN, ECHO_PIN);
+   if (distance > 10 && distance < 50) {
+    Serial.println(str(distance) + "см");
+    manipulator.write(45);
+    delay(2000);
+    manipulator.write(90);
+    delay(2000);
+  }
+  delay(500);
+}
+
+long getDistance() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  
+  long duration = pulseIn(ECHO_PIN, HIGH);
+  long distance = duration * 0.034;
+  
+  return distance;
+}
