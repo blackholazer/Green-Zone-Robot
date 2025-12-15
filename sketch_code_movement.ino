e9eb7245a40af154b578b08544d0e179362ce83e
Артём Ходько <artemhodko178@gmail.com>
2025-12-15 20:55:29 +0300

    Update code_movement

--- sketch_code_movement.ino
+++ sketch_code_movement.ino
@@ -10,15 +10,15 @@ int R_MOTOR2 = 10
 int R_MOTOR2_DIR = 12
 
 void setup() {
-  pinMode(L_MOTOR1, OUTPUT)
-  pinMode(L_MOTOR1_DIR, OUTPUT)
-  pinMode(L_MOTOR2, OUTPUT)
-  pinMode(L_MOTOR2_DIR, OUTPUT)
+  pinMode(L_MOTOR1, OUTPUT);
+  pinMode(L_MOTOR1_DIR, OUTPUT);
+  pinMode(L_MOTOR2, OUTPUT);
+  pinMode(L_MOTOR2_DIR, OUTPUT);
 
   pinMode(R_MOTOR1, OUTPUT)
-  pinMode(R_MOTOR1_DIR, OUTPUT)
-  pinMode(R_MOTOR2, OUTPUT)
-  pinMode(R_MOTOR2_DIR, OUTPUT)
+  pinMode(R_MOTOR1_DIR, OUTPUT);
+  pinMode(R_MOTOR2, OUTPUT);
+  pinMode(R_MOTOR2_DIR, OUTPUT);
 
   Serial.begin(9600)
 }
@@ -30,27 +30,27 @@ void loop() {
 
 
 void move_foward(speed){
-  analogWrite(L_MOTOR1, speed)
-  analogWrite(L_MOTOR2, speed)
-  digitalWrite(L_MOTOR1_DIR, HIGH)
-  digitalWrite(L_MOTOR2_DIR, HIGH)
+  analogWrite(L_MOTOR1, speed);
+  analogWrite(L_MOTOR2, speed);
+  digitalWrite(L_MOTOR1_DIR, HIGH);
+  digitalWrite(L_MOTOR2_DIR, HIGH);
 
-  analogWrite(R_MOTOR1, speed)
-  analogWrite(R_MOTOR2, speed)
-  digitalWrite(R_MOTOR1_DIR, HIGH)
-  digitalWrite(R_MOTOR2_DIR, HIGH)
+  analogWrite(R_MOTOR1, speed);
+  analogWrite(R_MOTOR2, speed);
+  digitalWrite(R_MOTOR1_DIR, HIGH);
+  digitalWrite(R_MOTOR2_DIR, HIGH);
 
 }
 
 void move_backward(speed){
-  analogWrite(L_MOTOR1, speed)
-  analogWrite(L_MOTOR2, speed)
-  digitalWrite(L_MOTOR1_DIR, LOW)
-  digitalWrite(L_MOTOR2_DIR, LOW)
+  analogWrite(L_MOTOR1, speed);
+  analogWrite(L_MOTOR2, speed);
+  digitalWrite(L_MOTOR1_DIR, LOW);
+  digitalWrite(L_MOTOR2_DIR, LOW);
 
-  analogWrite(R_MOTOR1, speed)
-  analogWrite(R_MOTOR2, speed)
-  digitalWrite(R_MOTOR1_DIR, LOW)
-  digitalWrite(R_MOTOR2_DIR, LOW)
+  analogWrite(R_MOTOR1, speed);
+  analogWrite(R_MOTOR2, speed);
+  digitalWrite(R_MOTOR1_DIR, LOW);
+  digitalWrite(R_MOTOR2_DIR, LOW);
 
 }
